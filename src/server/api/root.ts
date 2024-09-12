d2a84e13cf324e5a5ff3ab207bc36f34e1ed551c
va_ng <vanessaw2309@live.com>
2024-09-12 23:12:09 +1000

    fixing errors

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "t3/server/api/routers/post";
+// import { postRouter } from "t3/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "t3/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "t3/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  // post: postRouter,
 });
 
 // export type definition of API
@@ -21,3 +21,48 @@ export type AppRouter = typeof appRouter;
  *       ^? Post[]
  */
 export const createCaller = createCallerFactory(appRouter);
+
+// POST>TS
+
+// import { z } from "zod";
+
+// import {
+//   createTRPCRouter,
+//   protectedProcedure,
+//   publicProcedure,
+// } from "t3/server/api/trpc";
+
+// export const postRouter = createTRPCRouter({
+//   hello: publicProcedure
+//     .input(z.object({ text: z.string() }))
+//     .query(({ input }) => {
+//       return {
+//         greeting: `Hello ${input.text}`,
+//       };
+//     }),
+
+//   create: protectedProcedure
+//     .input(z.object({ name: z.string().min(1), content: z.string() }))
+//     .mutation(async ({ ctx, input }) => {
+//       return ctx.db.post.create({
+//         data: {
+//           name: input.name,
+//           content: input.content, // Add this line
+//           createdBy: { connect: { id: ctx.session.user.id } },
+//         },
+//       });
+//     }),
+
+//   getLatest: protectedProcedure.query(async ({ ctx }) => {
+//     const post = await ctx.db.post.findFirst({
+//       orderBy: { createdAt: "desc" },
+//       where: { createdBy: { id: ctx.session.user.id } },
+//     });
+// // 
+//     return post ?? null;
+//   }),
+
+//   getSecretMessage: protectedProcedure.query(() => {
+//     return "you can now see this secret message!";
+//   }),
+// });
