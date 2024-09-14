f206e51899b05d2158a35b286602ed3138fd38ca
vang0130 <vanessaw2309@live.com>
2024-09-15 03:37:26 +1000

    blog post ability and addition of tags

--- src/app/sidebar/page.tsx
+++ src/app/sidebar/page.tsx
@@ -1,9 +1,10 @@
 
 import SideBarClient from './sideBar';
+
 // import { SessionProvider } from 'next-auth/react';
 import { useSession } from 'next-auth/react';
 
 export default function SideBar() {
 
-  return <SideBar/>;
+  return <SideBarClient/>;
 }
